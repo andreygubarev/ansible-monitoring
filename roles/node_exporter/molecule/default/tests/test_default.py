fe373a3c964daa567486f727ed0eb38888f2e43e
Andrey Gubarev <andrey@andreygubarev.com>
2023-05-27 10:54:24 +0400

    add node exporter role

--- roles/node_exporter/molecule/default/tests/test_default.py
+++ roles/node_exporter/molecule/default/tests/test_default.py
@@ -1,10 +1,16 @@
 """Role testing files using testinfra."""
 
 
-def test_hosts_file(host):
-    """Validate /etc/hosts file."""
-    f = host.file("/etc/hosts")
+def test_node_exporter_service(host):
+    """Validate node_exporter service."""
+    s = host.service("node_exporter")
 
-    assert f.exists
-    assert f.user == "root"
-    assert f.group == "root"
+    assert s.is_enabled
+    assert s.is_running
+
+
+def test_node_exporter_socket(host):
+    """Validate node_exporter socket."""
+    s = host.socket("tcp://0.0.0.0:9100")
+
+    assert s.is_listening
